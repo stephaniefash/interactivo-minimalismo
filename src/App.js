ce8ba4d5e2800c9169d2e780212e780381132dcc
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-05-20 19:18:01 +0100

    adds animation for side links

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ const links = ["contacts", "creations", "about", "John Doe"];
 const rightLinks = ["Scroll", "---------------", "Contact me?"];
 
 const showLinks = (links) => {
-    return links.map( link => <li> {link} </li>)
+    return links.map( link => <li className="single-link"> {link} </li>)
 };
 
 function App() {
   return (
     <div className="app-div">
         <li id="text-left" className="side-text-left list-item">
-            <ul><a href="www.google.com" className="single-link">{showLinks(links)}</a></ul>
+            <ul><a href="www.google.com" className="links">{showLinks(links)}</a></ul>
         </li>
 
-        <li id="text-right" className="side-text-right">
-            <ul><a href="www.google.com" className="single-link">{showLinks(rightLinks)}</a></ul>
+        <li id="text-right" className="side-text-right list-item">
+            <ul><a href="www.google.com" className="links">{showLinks(rightLinks)}</a></ul>
         </li>
     </div>
   );
