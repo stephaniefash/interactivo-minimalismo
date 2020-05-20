74e382bdde505bb7f0c38604929ae83617f7903e
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-05-20 18:33:51 +0100

    Adds side links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const links = ["contacts", "creations", "about", "John Doe"];
+const rightLinks = ["Scroll", "---------------", "Contact me?"];
+
+const showLinks = (links) => {
+    return links.map( link => <li> {link} </li>)
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-div">
+        <li id="text-left" className="side-text-left list-item">
+            <ul><a href="www.google.com" className="single-link">{showLinks(links)}</a></ul>
+        </li>
+
+        <li id="text-right" className="side-text-right">
+            <ul><a href="www.google.com" className="single-link">{showLinks(rightLinks)}</a></ul>
+        </li>
     </div>
   );
 }
